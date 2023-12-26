5295f60ff95867066faa94cac62905604e1c9b1b
coconutpalmtreeisland <adorableness1215@naver.com>
2023-12-26 15:09:36 +0900

    23.12.26 로그인html

---
+++ assets/js/swiper.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "fade",
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
